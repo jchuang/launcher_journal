241e161c10e9186464ccf7fd76b1990847f2f298
Janet Chuang <janet.chuang@gmail.com>
2013-12-23 18:27:48 -0500

    add category drop-down menu to entry form

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -49,6 +49,6 @@ class EntriesController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def entry_params
-    params.require(:entry).permit(:title, :content)
+    params.require(:entry).permit(:title, :content, :category_id)
   end
 end

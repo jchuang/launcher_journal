b18b4ecda70eda4501966aaab5a771a692cb1c22
Janet Chuang <janet.chuang@gmail.com>
2013-12-22 22:22:32 -0500

    add index view for entries, with date created and title

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,28 @@
+class EntriesController < ApplicationController
+  before_action :set_entry, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @entries = Entry.all.order(created_at: :desc)
+  end
+
+  def show
+  end
+
+  def new
+    @entry = Entry.new
+  end
+
+  def edit
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_entry
+      @entry = Entry.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def entry_params
+      params.require(:entry).permit(:title, :content)
+    end
+end
